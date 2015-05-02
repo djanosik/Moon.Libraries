31dba6d0bd694e404b069717223233f1f828c18c
Dusan Janosik <dusan@djanosik.cz>
2015-05-02 11:26:56 +0200

    Updates build script to download latest version of DNX.

--- build.cmd
+++ build.cmd
@@ -16,8 +16,13 @@ copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
 IF EXIST packages\KoreBuild goto run
-.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
-.nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
+.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -source "https://www.myget.org/F/aspnetrelease/api/v2" -nocache -pre
+.nuget\NuGet.exe install Sake -version 0.2 -ExcludeVersion -o packages
+
+IF "%SKIP_DNX_INSTALL%"=="1" goto run
+CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
+CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
 
 :run
+CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
 packages\Sake\tools\Sake.exe -I packages\KoreBuild\build -f makefile.shade %*
