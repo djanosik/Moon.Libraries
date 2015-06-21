5f03406d9acfc354552a421a1452607ff08032be
Dusan Janosik <dusan@djanosik.cz>
2015-06-21 19:45:34 +0200

    Updates project files, includes more metadata in assemblies.

--- src/Moon.IO.FileSystem/Properties/AssemblyInfo.cs
+++ src/Moon.IO.FileSystem/Properties/AssemblyInfo.cs
@@ -1,3 +1,9 @@
 using System.Reflection;
+#if !DNXCORE50
+using System.Resources;
+#endif
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
+#if !DNXCORE50
+[assembly: NeutralResourcesLanguage("en-US")]
+#endif
