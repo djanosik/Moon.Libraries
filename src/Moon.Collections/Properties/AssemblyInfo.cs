eb97b80fe756c6da977a162523e674725afafe45
Dusan Janosik <dusan@djanosik.cz>
2015-09-03 16:02:52 +0200

    Updates assembly attributes.

--- src/Moon.Collections/Properties/AssemblyInfo.cs
+++ src/Moon.Collections/Properties/AssemblyInfo.cs
@@ -1,9 +1,5 @@
 using System.Reflection;
-#if !DNXCORE50
 using System.Resources;
-#endif
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
-#if !DNXCORE50
 [assembly: NeutralResourcesLanguage("en-US")]
-#endif
